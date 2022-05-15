26c27ac92f6975a4b33bdb4c8e8883fc55f0190a
nep <3368939477@qq.com>
2022-05-15 20:15:15 +0800

    5.15
    Signed-off-by: nep <3368939477@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Code-training
 关于 javascript 和 rust 的代码例子训练，来源 codewars
+
+- 5.15
+
+  ```javascript
+      //' 为什么是 == 或者 ==== 之间有什么区别
+      //- 当使用 == 时会进行数据类型的转换,0=false，而 === 会直接进行数据类型之间的比较
+  ```
+
+  
---
+++ javascript/5.15.js
@@ -0,0 +1,11 @@
+//! 判断数是否为浮点数
+function testEven(n) {
+    return n % 2 == 0
+    //' 为什么是 == 或者 ==== 之间有什么区别
+    //- 当使用 == 时会进行数据类型的转换,0=false，而 === 会直接进行数据类型之间的比较
+}
+if (testEven(0.5) == true) {
+    console.log("正确的");
+} else {
+    console.log("错误的");
+}

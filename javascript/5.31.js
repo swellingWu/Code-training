46ea8aa9b92c8886c73aa661076a31be9a4c3adf
nep <3368939477@qq.com>
2022-05-31 11:16:06 +0800

    5.31
    Signed-off-by: nep <3368939477@qq.com>

---
+++ javascript/5.31.js
@@ -0,0 +1,16 @@
+let index = length = 10;
+
+if(index !== length){
+    console.log("!== is true");
+  }
+  if(index === length){
+  }else{
+    console.log("!== is false");
+  }
+
+
+  if (index === length) {
+        console.log("=== is true");
+  } else {
+    console.log("=== is false");
+  }
